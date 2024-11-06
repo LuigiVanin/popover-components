e936a943a275b30671fce32824ceb1c73182242c
LuigiVanin <luisfvaninmartins@gmail.com>
2024-11-06 02:05:31 -0300

    fix tsconfig error

--- packages/ui/src/MyBrandButton/MyBrandButton.vue
+++ packages/ui/src/MyBrandButton/MyBrandButton.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="bg-brand-main px-2 py-1 shadow-sm hover:bg-opacity-90 hover:shadow-md rounded-sm">
+  <button
+    class="bg-brand-main rounded-sm px-2 py-1 shadow-sm hover:bg-opacity-90 hover:shadow-md"
+  >
     <slot />
   </button>
 </template>
