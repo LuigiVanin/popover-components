212354f352f9ab81eb42a30a8e26b97d12ccfc03
LuigiVanin <luisfvaninmartins@gmail.com>
2024-12-22 23:08:29 -0300

    fix: small adjustments on package json and vite config

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -6,8 +6,8 @@ import dts from "vite-plugin-dts";
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    // outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: false,
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
 
     lib: {
       entry: path.resolve(__dirname, "src/main.ts"),
